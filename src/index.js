277d411e3e38be3b6a6703643ec9db6ecdabce81
nastya-koptilova <koptilova.nastya@gmail.com>
2022-11-27 00:14:54 +0200

    add mobile modal

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
   const refs = {
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
+    closeMenuBtnBuy: document.querySelector('[data-menu-close-btn]'),
     menu: document.querySelector('[data-menu]'),
     body: document.querySelector('body'),
     menuList: document.querySelector('.header__mobile-list'),
@@ -9,6 +10,7 @@
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtnBuy.addEventListener('click', toggleMenu);
   refs.menuList.addEventListener('click', removeMenu);
 
   function toggleMenu() {
--- src/js/buy-now.js
+++ src/js/buy-now.js
@@ -1,12 +1,14 @@
 (() => {
   const refs = {
     openModalBtnBuy: document.querySelector('[data-modal-open-buy-now]'),
+    openModalBtnBuyMob: document.querySelector('[data-modal-open-buy-now-mob]'),
     closeModalBtnBuy: document.querySelector('[data-modal-close-buy-now]'),
     modalBuy: document.querySelector('[data-modal-buy-now]'),
     body: document.querySelector('body'),
   };
 
   refs.openModalBtnBuy.addEventListener('click', toggleModalBuy);
+  refs.openModalBtnBuyMob.addEventListener('click', toggleModalBuy);
   refs.closeModalBtnBuy.addEventListener('click', toggleModalBuy);
 
   function toggleModalBuy() {
